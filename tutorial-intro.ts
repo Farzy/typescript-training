9c69e857b6653b1d08c95b284973e17b6f68a76a
Farzad FARID <farzy@farzy.org>
2022-11-19 21:57:59 +0100

    Add more samples

--- tutorial-intro.ts
+++ tutorial-intro.ts
@@ -81,10 +81,55 @@ interface Backpack<Type> {
 declare const backpack: Backpack<string>;
 
 // object is a string, because we declared it above as the variable part of Backpack.
-backpack.add("toto");
+// backpack.add("toto");
 
-const object = backpack.get();
-console.log("object: " + object);
+// const object = backpack.get();
+// console.log("object: " + object);
 
 // Since the backpack variable is a string, you can't pass a number to the add function.
 // backpack.add(23);
+
+// Structural type System
+// One of TypeScript’s core principles is that type checking focuses on the shape that values have. This is sometimes called “duck typing” or “structural typing”.
+//
+// In a structural type system, if two objects have the same shape, they are considered to be of the same type.
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+const point = { x: 12, y: 26};
+logPoint(point);
+
+// The point variable is never declared to be a Point type. However, TypeScript compares the shape of point to the shape of Point in the type-check. They have the same shape, so the code passes.
+//
+// The shape-matching only requires a subset of the object’s fields to match.
+const point3 = { x: 12, y: 26, z: 89 };
+logPoint(point3);
+
+const rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect);
+
+// Does not work
+const color = { hex: "#187ABF" };
+// logPoint(color);
+
+// There is no difference between how classes and objects conform to shapes:
+class VirtualPoint {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint);
